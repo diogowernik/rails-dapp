f30409d663c461cfa47867948f0325ca9e53e112
Diogo Wernik <11983260+diogowernik@users.noreply.github.com>
2023-02-09 19:32:51 -0300

    working on metamask connection

---
+++ app/javascript/controllers/metamask_controller.js
@@ -0,0 +1,90 @@
+import { Controller } from "@hotwired/stimulus";
+// import { ethers } from "ethers";
+
+// variables to store data
+let walletConnected = false;
+let userAddress = null;
+let userBalance = null;
+let provider = null;
+
+// contract on goerli that i deployed 0x5CFE10e919EE71274c971512514C94BfA78C923a
+
+// Connects to data-controller="metamask"
+export default class extends Controller {
+  static targets = [
+    "connect",
+    "wallet",
+    "results",
+    "form",
+    "address",
+    "balance",
+  ];
+
+  connect() {
+    this.isWalletConnected();
+  }
+
+  async isWalletConnected() {
+    if (window.ethereum) {
+      const accounts = await window.ethereum.request({
+        method: "eth_accounts",
+      });
+      this.syncComponents(accounts);
+    } 
+  }
+  
+  async connectMetamask() {
+    if (window.ethereum) {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      this.syncComponents(accounts);
+    } else {
+      console.log("Please install Metamask");
+    }
+  }
+
+  async syncComponents(accounts) {
+    // provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    if (accounts.length > 0) {
+      walletConnected = true;
+      userAddress = accounts[0];
+      console.log("your account address is: " + accounts[0]);
+      // hide connect button
+      this.connectTarget.hidden = true;
+      // show elements
+      this.walletTarget.hidden = false;
+      this.resultsTarget.hidden = false;
+      this.formTarget.hidden = false;
+
+      this.addressTarget.innerText = `${userAddress.slice(
+        0,
+        6
+      )}...${userAddress.slice(-3)}`.toUpperCase(); 
+      
+      // userBalance = await provider.getBalance(userAddress);
+      // userBalance = ethers.utils.formatEther(userBalance);
+      // this.balanceTarget.innerText = Math.round(userBalance * 10000) / 10000;
+      
+
+
+      // metamask event: reload page if account changes
+      window.ethereum.on("accountsChanged", (accounts) => {
+        window.location.reload();
+      });
+
+    } else {
+      walletConnected = false;
+      console.log("wallet not connected");
+      // show connect button
+      this.connectTarget.hidden = false;
+      // hide elements
+      this.walletTarget.hidden = true;
+      this.resultsTarget.hidden = true;
+      this.formTarget.hidden = true;
+      this.addressTarget.innerText = "";
+    }
+  }
+
+}
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,18 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "ethers", to: "https://ga.jspm.io/npm:ethers@6.0.1/lib.esm/index.js"
+pin "#lib.esm/crypto/crypto.js", to: "https://ga.jspm.io/npm:@jspm/core@2.0.0/nodelibs/@empty.js"
+pin "#lib.esm/providers/provider-ipcsocket.js", to: "https://ga.jspm.io/npm:@jspm/core@2.0.0/nodelibs/@empty.js"
+pin "#lib.esm/providers/ws.js", to: "https://ga.jspm.io/npm:@jspm/core@2.0.0/nodelibs/@empty.js"
+pin "#lib.esm/utils/base64.js", to: "https://ga.jspm.io/npm:@jspm/core@2.0.0/nodelibs/@empty.js"
+pin "#lib.esm/utils/geturl.js", to: "https://ga.jspm.io/npm:@jspm/core@2.0.0/nodelibs/@empty.js"
+pin "#lib.esm/wordlists/wordlists.js", to: "https://ga.jspm.io/npm:@jspm/core@2.0.0/nodelibs/@empty.js"
+pin "@adraffy/ens-normalize/xnf", to: "https://ga.jspm.io/npm:@adraffy/ens-normalize@1.8.9/dist/index-xnf.js"
+pin "@noble/hashes/crypto", to: "https://ga.jspm.io/npm:@noble/hashes@1.1.2/esm/cryptoBrowser.js"
+pin "@noble/hashes/ripemd160", to: "https://ga.jspm.io/npm:@noble/hashes@1.1.2/esm/ripemd160.js"
+pin "@noble/hashes/scrypt", to: "https://ga.jspm.io/npm:@noble/hashes@1.1.2/esm/scrypt.js"
+pin "@noble/hashes/sha3", to: "https://ga.jspm.io/npm:@noble/hashes@1.1.2/esm/sha3.js"
+pin "@noble/secp256k1", to: "https://ga.jspm.io/npm:@noble/secp256k1@1.7.1/lib/esm/index.js"
+pin "aes-js", to: "https://ga.jspm.io/npm:aes-js@4.0.0-beta.3/lib.esm/index.js"
+pin "crypto", to: "https://ga.jspm.io/npm:@jspm/core@2.0.0/nodelibs/browser/crypto.js"
